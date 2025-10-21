6d43a08a6a58e8f0a6f6a6aa438da849dfdcd5aa
latifaja <latifajakir44@gmail.com>
2025-10-21 16:44:59 +0100

    creation de getway-service

---
+++ gatewey-service/src/main/java/org/example/gateweyservice/GateweyServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gateweyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateweyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateweyServiceApplication.class, args);
+    }
+
+}
---
+++ gatewey-service/src/test/java/org/example/gateweyservice/GateweyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gateweyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateweyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
